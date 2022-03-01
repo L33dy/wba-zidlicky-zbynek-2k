5b479282a969e5adb898d1a3cd53bd70c9776df0
L33dy <zbynekzidlicky@gmail.com>
2022-03-01 15:26:02 +0100

    contact page done, js added for it

---
+++ individual_work/zidlicky-2k-web-figma-1/contact/js/form.js
@@ -0,0 +1,7 @@
+function HidePlaceholder(a) {
+    document.getElementById(a).style.visibility = "hidden";
+}
+
+function ShowPlaceHolder(a) {
+    document.getElementById(a).style.visibility = "visible";
+}
