8ce4a9ecbb9ff07664d6dac9837525b01b48c4cb
L33dy <zbynekzidlicky@gmail.com>
2022-03-02 23:57:03 +0100

    contact form improvement

--- individual_work/zidlicky-2k-web-figma-1/contact/js/form.js
+++ individual_work/zidlicky-2k-web-figma-1/contact/js/form.js
@@ -2,6 +2,17 @@ function HidePlaceholder(a) {
     document.getElementById(a).style.visibility = "hidden";
 }
 
-function ShowPlaceHolder(a) {
-    document.getElementById(a).style.visibility = "visible";
+function ShowPlaceHolder(holder, input) {
+    const inputValue = document.getElementById(input).value;
+    const value = inputValue.trim();
+
+
+    if(value === "") {
+        document.getElementById(holder).style.visibility = "visible";
+    }
 }
+
+function toHomePage() {
+    window.location.href = "../index.html";
+}
+
